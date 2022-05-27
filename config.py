c5feab4bd70996b4d4388dab1ca9164ec1a5f648
gokul-gopan-k <gokul.gopan.k@gmail.com>
2022-05-27 20:28:07 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+class CONFIG:
+    
+    IMG_PATH = "../input/face-mask-segmentation-dataset/images"
+    MASK_PATH = "../input/face-mask-segmentation-dataset/masks"
+    BATCH_SIZE= 2
+    IMG_CHANNEL_IMAGE = 3
+    IMG_CHANNEL_MASK = 1
+    IMG_HEIGHT = 128
+    IMG_WIDTH = 128
+    EPOCHS =10
+    train_test_split =0.1
+    NO_OF_TEST_SAMPLES = 5
+
+
